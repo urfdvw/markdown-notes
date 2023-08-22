b8a9e67a1e375b5464a870468a17ab36018f4204
术生何 <urfdvw@gmail.com>
2023-08-21 21:11:18 -0400

    FlexLayout init

---
+++ log/readme.md
@@ -0,0 +1,4 @@
+# packages
+- `npm create vite@latest .`
+- `npm install vite-plugin-singlefile --save-dev`
+- `npm install flexlayout-react`
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,63 @@
 import { useState } from "react";
+import * as FlexLayout from "flexlayout-react";
+
+var json = {
+    global: {},
+    layout: {
+        type: "row",
+        weight: 100,
+        children: [
+            {
+                type: "tabset",
+                weight: 50,
+                selected: 0,
+                children: [
+                    {
+                        type: "tab",
+                        name: "One",
+                        component: "panel",
+                    },
+                ],
+            },
+            {
+                type: "tabset",
+                weight: 50,
+                selected: 0,
+                children: [
+                    {
+                        type: "tab",
+                        name: "Two",
+                        component: "panel",
+                    },
+                ],
+            },
+            {
+                type: "tabset",
+                weight: 50,
+                selected: 0,
+                children: [
+                    {
+                        type: "tab",
+                        name: "Three",
+                        component: "panel",
+                    },
+                ],
+            },
+        ],
+    },
+};
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [model, setModel] = useState(FlexLayout.Model.fromJson(json));
 
-    return (
-        <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-        </button>
-    );
+    const factory = (node) => {
+        var component = node.getComponent();
+        if (component === "panel") {
+            return <div className="tab_content">{node.getName()}</div>;
+        }
+    };
+
+    return <FlexLayout.Layout model={model} factory={factory} />;
 }
 
 export default App;
