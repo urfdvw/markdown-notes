7703cba1e8ea552f3ced861ab6ba8160eaeb97bd
River Wang <urfdvw@gmail.com>
2023-08-22 19:04:08 -0400

    ace in tabs

--- log/readme.md
+++ log/readme.md
@@ -1,10 +1,16 @@
 # packages
-- `npm create vite@latest .`
-- `npm install vite-plugin-singlefile --save-dev`
-- `npm install flexlayout-react`
-    - use simple as example, and running well, and compile small
-- add git build action and see what is the built version
-    - copy `.github`
-    - add `base` to `vite.config.js`
-    - it seems like if I build the one-file version, the github page is also one file version
-        - not critical for this usage
+
+-   `npm create vite@latest .`
+-   `npm install vite-plugin-singlefile --save-dev`
+-   `npm install flexlayout-react`
+    -   use simple as example, and running well, and compile small
+-   add git build action and see what is the built version
+    -   copy `.github`
+    -   add `base` to `vite.config.js`
+    -   it seems like if I build the one-file version, the github page is also one file version
+        -   not critical for this usage
+-   `npm install react-ace ace-builds`
+
+# insights
+
+-   top border divide the screen first
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import { useState } from "react";
 import * as FlexLayout from "flexlayout-react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-cloud9_day";
+import "ace-builds/src-noconflict/ext-language_tools";
 
 var json = {
-    global: {},
+    global: { tabEnableClose: false },
+    borders: [
+        {
+            type: "border",
+            location: "left",
+            size: 100,
+            children: [
+                {
+                    type: "tab",
+                    name: "five",
+                    component: "panel",
+                },
+            ],
+        },
+        {
+            type: "border",
+            location: "bottom",
+            size: 100,
+            children: [
+                {
+                    type: "tab",
+                    name: "four",
+                    component: "panel",
+                },
+            ],
+        },
+    ],
     layout: {
         type: "row",
         weight: 100,
@@ -29,13 +59,6 @@ var json = {
                         name: "Two",
                         component: "panel",
                     },
-                ],
-            },
-            {
-                type: "tabset",
-                weight: 50,
-                selected: 0,
-                children: [
                     {
                         type: "tab",
                         name: "Three",
@@ -53,7 +76,18 @@ function App() {
     const factory = (node) => {
         var component = node.getComponent();
         if (component === "panel") {
-            return <div className="tab_content">{node.getName()}</div>;
+            return (
+                <div className="tab_content">
+                    <AceEditor
+                        mode="python"
+                        theme="cloud9_day"
+                        name="UNIQUE_ID_OF_DIV"
+                        editorProps={{ $blockScrolling: true }}
+                        width="100%"
+                        height="100%"
+                    />
+                </div>
+            );
         }
     };
 
