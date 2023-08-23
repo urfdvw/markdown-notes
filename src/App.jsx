ecab95e6219347fb2a54162dbf1d64fef2d753f7
术生何 <urfdvw@gmail.com>
2023-08-22 22:08:19 -0400

    move layout to json

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,13 @@
 import { useState } from "react";
 import * as FlexLayout from "flexlayout-react";
 import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-markdown";
 import "ace-builds/src-noconflict/theme-cloud9_day";
 import "ace-builds/src-noconflict/ext-language_tools";
-
-var json = {
-    global: { tabEnableClose: false },
-    borders: [
-        {
-            type: "border",
-            location: "left",
-            size: 100,
-            children: [
-                {
-                    type: "tab",
-                    name: "five",
-                    component: "panel",
-                },
-            ],
-        },
-        {
-            type: "border",
-            location: "bottom",
-            size: 100,
-            children: [
-                {
-                    type: "tab",
-                    name: "four",
-                    component: "panel",
-                },
-            ],
-        },
-    ],
-    layout: {
-        type: "row",
-        weight: 100,
-        children: [
-            {
-                type: "tabset",
-                weight: 50,
-                selected: 0,
-                children: [
-                    {
-                        type: "tab",
-                        name: "One",
-                        component: "panel",
-                    },
-                ],
-            },
-            {
-                type: "tabset",
-                weight: 50,
-                selected: 0,
-                children: [
-                    {
-                        type: "tab",
-                        name: "Two",
-                        component: "panel",
-                    },
-                    {
-                        type: "tab",
-                        name: "Three",
-                        component: "panel",
-                    },
-                ],
-            },
-        ],
-    },
-};
+import layout from "./layout.json";
 
 function App() {
-    const [model, setModel] = useState(FlexLayout.Model.fromJson(json));
+    const [model, setModel] = useState();
 
     const factory = (node) => {
         var component = node.getComponent();
@@ -79,7 +15,7 @@ function App() {
             return (
                 <div className="tab_content">
                     <AceEditor
-                        mode="python"
+                        mode="markdown"
                         theme="cloud9_day"
                         name="UNIQUE_ID_OF_DIV"
                         editorProps={{ $blockScrolling: true }}
@@ -91,7 +27,12 @@ function App() {
         }
     };
 
-    return <FlexLayout.Layout model={model} factory={factory} />;
+    return (
+        <FlexLayout.Layout
+            model={FlexLayout.Model.fromJson(layout)}
+            factory={factory}
+        />
+    );
 }
 
 export default App;
